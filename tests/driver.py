2f0ebdc534f47fbe010aa8fc52dc8f2b4d45e6fd
25077667 <zxc25077667@gmail.com>
2022-04-26 00:35:06 +0800

    Finish unit test driver

--- tests/driver.py
+++ tests/driver.py
@@ -5,13 +5,25 @@ import re
 
 def get_all_units() -> list():
     l = []
-    for _, _, files in os.walk("./tests/", topdown=False):
-        for name in files:
-            if(re.search("^.*.o$", name)):
-                l.append(name)
+    cur_name = os.getcwd()
+    for root, _, files in os.walk("./tests/", topdown=False):
+        for f in files:
+            if(re.search("^.*.o$", f)):
+                fullpath = cur_name + root[1:] + "/" + f
+                l.append(fullpath)
     return l
+
+
+def exec_test(files: str) -> bool:
+    for f in files:
+        print("Test " + f)
+        if(os.system(f)):   # Return non-zero if failed
+            print("\n\nError on: \x1b[1;31m{}\x1b[0m\n\n".format(f))
+            return False
+    return True
 
 
 if __name__ == '__main__':
     files = get_all_units()
-    print(files)
+    if not exec_test(files):
+        exit(1)
